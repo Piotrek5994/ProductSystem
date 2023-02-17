e7a454c70c45433b1b870d1d14ba9ef831a68b96
Piotrek5994 <piotrek5994@gmail.com>
2023-02-17 18:42:25 +0100

    End xUnit Test

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -42,6 +42,84 @@ namespace TestProject1
             Assert.Equal("Milk", product2.Description);
         }
 
+        [Fact]
+        public void Test4()
+        {
+            //give
+            var productService = new ProductsService();
+            var products = productService.AddNewProduct("Milk", "Milk", 5.00);
+            var products2 = productService.AddNewProduct("Cake", "Cake", 10.00);
+            //when
+            var product3 = productService.GetAllProducts();
+            //then
+            Assert.Equal(2, product3.Count());
+        }
 
+        [Fact]
+        public void Test5()
+        {
+            //give
+            var productService = new ProductsService();
+            var products = productService.AddNewProduct("Milk", "Milk", 5.00);
+            var products2 = productService.AddNewProduct("Cake", "Cake", 10.00);
+            //when
+            var product3 = productService.GetProduct("Milk");
+            //then
+            Assert.Equal("Milk", product3.Name);
+            Assert.Equal("Milk", product3.Description);
+        }
+
+        [Fact]
+        public void Test6()
+        {
+            //give
+            var productService = new ProductsService();
+            var products = productService.AddNewProduct("Milk", "Milk", 5.00);
+            var products2 = productService.AddNewProduct("Cake", "Cake", 10.00);
+            //when
+            var product3 = productService.GetProduct("Cake");
+            //then
+            Assert.Equal("Cake", product3.Name);
+            Assert.Equal("Cake", product3.Description);
+        }
+
+        [Fact]
+        public void Test7()
+        {
+          //give
+            var productService = new ProductsService();
+            var products = productService.AddNewProduct("Milk", "Milk", 5.00);;
+            var products2 = productService.AddNewProduct("Cake", "Cake", 10.00);
+            //when
+            var product3 = productService.GetLastModifiedProduct();
+            //then
+            Assert.Equal("Cake", product3.Name);
+
+        }
+
+        [Fact]
+        public void Test8()
+        {
+            //give
+            var productService = new ProductsService();
+            var products3 = productService.AddNewProduct("Milk", "Milk", 5.00);
+            var products2 = productService.AddNewProduct("Cake", "Cake", 10.00);
+            //when
+            var product4 = productService.GetCheapestProduct();
+            //then
+            Assert.Equal("Milk", product4.Name);
+        }
+        [Fact]
+        public void Test9()
+        {
+            //give
+            var productService = new ProductsService();
+            var products3 = productService.AddNewProduct("Milk", "Milk", 5.00);
+            var products2 = productService.AddNewProduct("Cake", "Cake", 10.00);
+            //when
+            var product4 = productService.GetMostExpensiveProduct();
+            //then
+            Assert.Equal("Cake", product4.Name);
+        }
     }
 }
